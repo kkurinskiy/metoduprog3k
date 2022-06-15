7ae0c18f2f880779fe244035f30a6a93b2f6c838
kkurinskiy <96626932+kkurinskiy@users.noreply.github.com>
2022-06-15 18:52:37 +0300

    Add files via upload

---
+++ 2.py
@@ -0,0 +1,16 @@
+N, M = map(int, input().split())
+Edges = []
+for i in range(M):
+    start, end, weight = map(int, input().split())
+    Edges.append([weight, start, end])
+Edges.sort()
+Comp = [i for i in range(N)]
+Ans = 0
+for weight, start, end in Edges:
+    if Comp[start] != Comp[end]:
+        Ans += weight
+        a = Comp[start]
+        b = Comp[end]
+        for i in range(N):
+            if Comp[i] == b:
+                Comp[i] = a
